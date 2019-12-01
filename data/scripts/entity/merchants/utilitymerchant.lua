f05e6cf356d79ce5098721e253eb44c25d43c772
SmotPokin42000 <schroeder2593@gmail.com>
2019-12-01 00:16:44 -0700

    Delete utilitymerchant.lua

--- data/scripts/entity/merchants/utilitymerchant.lua
+++
@@ -1,8 +0,0 @@
-function UtilityMerchant.shop:addItems()
-    local item = UsableInventoryItem("reinforcementstransmitter.lua", Rarity(RarityType.Exotic), Faction().index)
-    UtilityMerchant.add(item, getInt(1, 3))
-    local item = UsableInventoryItem("equipmentmerchantcaller.lua", Rarity(RarityType.Exceptional), Faction().index)
-    UtilityMerchant.add(item, getInt(1, 9))
-    local item = UsableInventoryItem("energysuppressor.lua", Rarity(RarityType.Exceptional))
-    UtilityMerchant.add(item, getInt(1, 20))
-end
